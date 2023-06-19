a877a815bc711f5bd3f50f995485d4be32e518c9
LiaGaray2022 <104543168+LiaGaray2022@users.noreply.github.com>
2023-06-19 10:55:00 -0600

    ultimos cambios

---
+++ Prueba1Poke/Prueba1Poke/Models/Pokemon.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prueba1Poke.Models
+{
+    public class Pokemon
+    {
+        public string Nombre { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+
+        public Pokemon(string nombre, DateTime fechaNacimiento)
+        {
+            Nombre = nombre;
+            FechaNacimiento = fechaNacimiento;
+        }
+
+        public virtual string ObtenerSiguienteEvolucion()
+        {
+            return "";
+        }
+
+        public virtual Pokemon CrearEvolucion()
+        {
+            return null;
+        }
+
+        public virtual void ImprimirDatos()
+        {
+            Console.WriteLine("Nombre: " + Nombre);
+            Console.WriteLine("Fecha de nacimiento: " + FechaNacimiento.ToString("yyyy-MM-dd"));
+        }
+    }
+}
